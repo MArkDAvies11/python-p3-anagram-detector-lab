7375b01c4f1ff2a0830917ddb7042f15aacf3f9d
Mark Davies <markmuiruri11@gmail.com>
2025-08-12 14:55:15 +0300

    Completed Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, candidates):
+        anagrams = []
+        for candidate in candidates:
+            # Skip the word if it's identical to the original (case-insensitive)
+            if self.word == candidate.lower():
+                continue
+
+            # Compare the sorted letters of the candidate to the original word's sorted letters
+            if self.sorted_word == sorted(candidate.lower()):
+                anagrams.append(candidate)
+        
+        return anagrams
